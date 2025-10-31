260cc9562c636204f479a394aae0b15c5ecf134e
FLClover <ksj031012379@gmail.com>
2025-10-31 09:36:33 +0900

    feat:sellerHeader

---
+++ src/components/molecules/sellerHeader.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import Button from "../../components/atoms/button";
+
+const sellerHeader = ({ onSellClick }) => {
+  return (
+    <div className="flex justify-between items-center w-full pb-5 border-b border-white">
+      {/* 왼쪽 텍스트 */}
+      <div className="flex items-center text-white text-[62px] font-normal tracking-[-1.86px]">
+        나의 판매 포토카드
+      </div>
+    </div>
+  );
+};
+
+export default sellerHeader;
