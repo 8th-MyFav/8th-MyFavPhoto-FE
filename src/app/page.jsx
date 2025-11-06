0aef8b3d201ff70b2b5333aa19958bef1cb5e0ea
Aria Na <najuhyun.aria@gmail.com>
2025-11-06 16:41:51 +0900

    refactor: 배경 이미지 수정 및 위치 조정

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,46 +17,49 @@ const HomePage = () => {
       <div>
         <main>
           <div>
-            <div className="bg-black">
-              <div className="flex justify-center">
-                <img
-                  src="/images/favorite.svg"
-                  alt="로고"
-                  className="absolute pt-[77px] pb-[22px] w-[138.945px] z-1"
-                />
-              </div>
-              <div className="flex justify-center">
-                <div className="absolute pt-[125px] leading-snug ">
-                  <h1 className="text-noto-xl font-bold text-white align-center text-center pb-[33px]">
-                    구매하기 어려웠던
-                    <br />
-                    <span className="text-main text-bold text-noto-xl text-center">
-                      나의 최애
-                    </span>
-                    가 여기에!
-                  </h1>
-                  <div className="flex justify-center transition hover:shadow-lg">
-                    <Button
-                      text="최애 찾으러 가기"
-                      width="235px"
-                      height="55px"
-                      backgroundColor="var(--color-main)"
-                      color="var(--color-black)"
-                      fontSize="16px"
-                      padding="0"
-                      onClick={handleFindFav}
-                      className="transition transform hover:scale-105"
-                    />
+            <div className="relative w-screen h-[68rem]">
+              <img
+                src="/images/hpMain001.svg"
+                alt="메인 이미지"
+                className="w-screen absolute bottom-0 z-10"
+              />
+              <div className="absolute top-0 left-0 right-0 z-20">
+                <div className="flex justify-center">
+                  <img
+                    src="/images/favorite.svg"
+                    alt="로고"
+                    className="pt-[77px] pb-0 w-[138.945px]"
+                  />
+                </div>
+                <div className="flex justify-center">
+                  <div className="pt-[22.5px] leading-snug">
+                    <h1 className="text-noto-xl font-bold text-white align-center text-center pb-[33px]">
+                      구매하기 어려웠던
+                      <br />
+                      <span className="text-main text-bold text-noto-xl text-center">
+                        나의 최애
+                      </span>
+                      가 여기에!
+                    </h1>
+                    <div className="flex justify-center transition hover:shadow-lg">
+                      <Button
+                        text="최애 찾으러 가기"
+                        width="235px"
+                        height="55px"
+                        backgroundColor="var(--color-main)"
+                        color="var(--color-black)"
+                        fontSize="16px"
+                        padding="0"
+                        onClick={handleFindFav}
+                        className="transition transform hover:scale-105"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="mt-sm">
-              <img
-                src="/images/hpMain001.svg"
-                alt="배경"
-                className="z-0 w-screen"
-              />
+              <div className="absolute top-2xs bottom-0 left-0 right-0 mt-[13px] mx-lg z-0">
+                <div className="w-full h-[1088px] rounded-[30px] bg-gradient-to-tr from-[#445b6d] via-[#3f3f3f]/80 to-[#75426b]" />
+              </div>
             </div>
             <div>
               <div className="pl-[428px] pt-[133px] pb-md">
