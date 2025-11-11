3a49550427fdc7c5633b33f49292149e9a281a7b
Aria Na <najuhyun.aria@gmail.com>
2025-11-12 00:15:13 +0900

    fix: add dropdown border

--- src/components/molecules/DropDown.jsx
+++ src/components/molecules/DropDown.jsx
@@ -15,6 +15,7 @@ const Dropdown = ({
   enableReset = false,
   resetLabel = "전체",
   resetValue = "",
+  containerBorder,
   customStyles = {},
   onChange = () => {},
   value,
@@ -81,6 +82,7 @@ const Dropdown = ({
         padding,
         userSelect: "none",
         ...customStyles.container,
+        ...(containerBorder ? { border: containerBorder } : {}),
       }}
     >
       {/* 선택 영역 */}
