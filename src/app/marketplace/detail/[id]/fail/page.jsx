8bbb904e89f385e875f8666d174d71436fab092c
sungminiioo <sungminiioo426@gmail.com>
2025-11-11 18:42:13 +0900

    fix: 주소 방식 변경

--- src/app/marketplace/detail/[id]/fail/page.jsx
+++ src/app/marketplace/detail/[id]/fail/page.jsx
@@ -1,26 +1,6 @@
-"use client";
-import ActionResultPage from "@/components/organisms/ActionResultPage";
-import { useSearchParams } from "next/navigation";
+import React from "react";
+import PurchaseFailPage from "./failPage";
 
-export default function PurchaseFailPage() {
-  const searchParams = useSearchParams();
-
-  const rarity = searchParams.get("rarity");
-  const title = searchParams.get("title");
-  const quantity = searchParams.get("quantity");
-
-  return (
-    <div className="bg-black m-auto">
-      <div className="bg-black min-h-screen flex items-center justify-center">
-        <ActionResultPage
-          type="구매"
-          result="실패"
-          rarity={rarity}
-          title={title}
-          quantity={quantity}
-          buttonText="마켓 플레이스로 돌아가기"
-        />
-      </div>
-    </div>
-  );
+export default function FailPage() {
+  return <PurchaseFailPage />;
 }
---
+++ src/app/marketplace/detail/[id]/success/successPage.jsx
@@ -0,0 +1,29 @@
+"use client";
+import ActionResultPage from "@/components/organisms/ActionResultPage";
+import { PATHNAME } from "@/constants";
+import { useSearchParams, useRouter } from "next/navigation";
+
+export default function PurchaseSuccessPage() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const rarity = searchParams.get("rarity");
+  const title = searchParams.get("title");
+  const quantity = searchParams.get("quantity");
+
+  return (
+    <div className="bg-black m-auto">
+      <div className="bg-black min-h-screen flex items-center justify-center">
+        <ActionResultPage
+          type="구매"
+          result="성공"
+          rarity={rarity}
+          title={title}
+          quantity={quantity}
+          buttonText="마이갤러리에서 확인하기"
+          pathAction={PATHNAME.MYGAL}
+        />
+      </div>
+    </div>
+  );
+}
