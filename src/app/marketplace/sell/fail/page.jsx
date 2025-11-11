a798c5d3745dbd597f18916b994fed8a4ceba5d9
apple <apple@apples-MacBook-Air.local>
2025-11-11 15:27:17 +0900

    fix: SSR 페이지로 수정

--- src/app/marketplace/sell/fail/page.jsx
+++ src/app/marketplace/sell/fail/page.jsx
@@ -1,25 +1,12 @@
-"use client";
-import ActionResultPage from "@/components/organisms/ActionResultPage";
-import { useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
+import SellFailPage from "./failPage";
 
-export default function SellFailPage() {
-  const searchParams = useSearchParams();
-  const rarity = searchParams.get("rarity");
-  const title = searchParams.get("title");
-  const quantity = searchParams.get("quantity");
-
+function sellFaile() {
   return (
-    <div className="bg-black m-auto">
-      <div className="bg-black min-h-screen flex items-center justify-center">
-        <ActionResultPage
-          type="판매 등록"
-          result="실패"
-          rarity={rarity}
-          title={title}
-          quantity={quantity}
-          buttonText="마켓 플레이스로 돌아가기"
-        />
-      </div>
-    </div>
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <SellFailPage />
+    </Suspense>
   );
 }
+
+export default sellFaile;
